6c73853688782b04c5c9e2e98686fc2e7f7d33f4
EmpsyInternational <165856526+EmpsyInternational@users.noreply.github.com>
2024-06-22 09:01:47 -0400

    Update saveInfo.controller.js

--- src/controllers/saveInfo.controller.js
+++ src/controllers/saveInfo.controller.js
@@ -4,7 +4,7 @@ const controller = {};
 
 controller.saveUser = async (req, res) => {
     try {
-        const { cedula_rif, nombre, tipo_aliado, telefono, email, direccion, password } = req.body;
+        const { cedula_rif, nombre, telefono, email, direccion, password } = req.body;
         console.log('Request body:', req.body);
 
         let user = await getUser({ cedula_rif });
@@ -12,7 +12,7 @@ controller.saveUser = async (req, res) => {
         if (user.code === 200) {
             return res.status(500).json({ message: 'Aliado Already Registered', status: false, code: 500 });
         } else if (user.code === 404) {
-            let result = await saveUser({ cedula_rif, nombre, tipo_aliado ,telefono, direccion, email, password });
+            let result = await saveUser({ cedula_rif, nombre ,telefono, direccion, email, password });
 
             if (result.status) {
                 return res.status(result.code).json(result);
